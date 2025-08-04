e0b361c1fc3877592d9d57b9fe37c6a4f1ed8a60
aungkaungsetkiki <kaunglay2024.kg@gmail.com>
2025-08-04 16:22:39 +0630

    Delete migrations directory

--- migrations/001_create_tasks_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS tasks (
-  id SERIAL PRIMARY KEY,
-  user_id BIGINT NOT NULL,
-  task TEXT NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW(),
-  completed BOOLEAN DEFAULT FALSE
-);
-
-CREATE INDEX IF NOT EXISTS tasks_user_id_idx ON tasks(user_id);
